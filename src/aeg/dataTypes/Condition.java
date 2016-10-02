d8e55d773a932e9df05f01c5ec427ae7653db1c8
Aydın Emre Güzel <aydin.guzel@ozu.edu.tr>
2016-10-02 12:17:35 +0300

    Create Condition.java

---
+++ src/aeg/dataTypes/Condition.java
@@ -0,0 +1,18 @@
+package aeg.dataTypes;
+
+public class Condition {
+	public FlowElement[] ais;
+	String ops[];
+	String opList[] = {"&&","||","<","<=",">",">=","==","!="};
+	public Condition (FlowElement[] ais,String[] ops) {
+		this.ais = ais;
+		this.ops = ops;
+	}
+	String conditionProducer (int startLatency) {
+		String con = ais[0].getTrack(startLatency);
+		for(int i = 0;i<ais.length-1;i++){
+			con = con + " " + ops[i] + " " + ais[i+1].getTrack(startLatency);;
+		}
+		return con;
+	}
+}
